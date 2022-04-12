52f7e8cb8ce7c0f61964cf9d67e6492ffeb2533f
Luca Pirovano <luca.pirovano.98@gmail.com>
2022-04-12 16:45:57 +0200

    feat: spring security config

---
+++ backend/src/main/java/it/polimi/hypermedia/backend/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package it.polimi.hypermedia.backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user")
+                .password(new PasswordEncoder().passwordEncoder().encode("password")).roles("USER");
+    }
+
+    protected void configure(HttpSecurity security) throws Exception {
+        security
+                .cors().and().csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and().httpBasic();
+    }
+}
