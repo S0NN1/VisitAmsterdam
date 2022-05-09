d6bd79798b3f24549902e3e39a380e41f0e6c911
Nicolò Sonnino <swagpino@gmail.com>
2022-05-09 15:00:24 +0200

    feat: init card

---
+++ hypermedia-amogus/components/CardItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div v-if="isFigure" class="card w-96 bg-base-100 shadow-xl">
+    <figure><img :src="object.image" alt="object.name"></figure>
+    <!--    <div class="card-body">-->
+    <!--      <h2 class="card-title">-->
+    <!--        Shoes!-->
+    <!--      </h2>-->
+    <!--      <p>If a dog chews shoes whose shoes does he choose?</p>-->
+    <!--      <div class="card-actions justify-end">-->
+    <!--        <button class="btn btn-primary">-->
+    <!--          Buy Now-->
+    <!--        </button>-->
+    <!--      </div>-->
+    <!--    </div>-->
+  </div>
+  <div v-else class="card w-96 bg-base-100 shadow-xl">
+    <figure><img src="https://via.placeholder.com/1920x1080" alt="Shoes"></figure>
+    <!--    <div class="card-body">-->
+    <!--      <h2 class="card-title">-->
+    <!--        Shoes!-->
+    <!--      </h2>-->
+    <!--      <p>If a dog chews shoes whose shoes does he choose?</p>-->
+    <!--      <div class="card-actions justify-end">-->
+    <!--        <button class="btn btn-primary">-->
+    <!--          Buy Now-->
+    <!--        </button>-->
+    <!--      </div>-->
+    <!--    </div>-->
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    isFigure: {
+      type: Boolean,
+      default: true
+    },
+    object: {
+      type: Object,
+      default: () => {
+      }
+    }
+  },
+  data () {
+    return {}
+  },
+  methods: {}
+})
+</script>
--- hypermedia-amogus/components/CarouselItem.vue
+++ hypermedia-amogus/components/CarouselItem.vue
@@ -1,37 +1,78 @@
 <template>
-  <div>
-    <div v-for="(image, index) in carouselImages" :key="index" class="carousel w-full">
-      <div id="image{{index}}" class="carousel-item w-full">
-        <img src="{{image.path}}" class="w-full">
+  <div v-if="isComplex">
+    <div class="carousel w-full">
+      <!--      <div-->
+      <!--        v-for="(image, index) in carouselImages"-->
+      <!--        :id="'imageCarousel' + index"-->
+      <!--        :key="index"-->
+      <!--        class="carousel-item w-full"-->
+      <!--      >-->
+      <!--        <img :src="image.path" class="w-full">-->
+      <!--      </div>-->
+      <CardItem />
+    </div>
+    <div class="flex justify-center w-full py-2 gap-2">
+      <a
+        v-for="(image, index) in carouselImages"
+        :key="'imageCarousel' + index"
+        :href="'#imageCarousel'+ index"
+        class="btn btn-circle btn-xs carousel-btn mt-5"
+        :class="{'btn-active': activeIndex===index, 'ml-5': index>0}"
+        @click="selectedIndex(index)"
+      />
+    </div>
+  </div>
+  <div v-else>
+    <div class="carousel w-full">
+      <div
+        v-for="(image, index) in carouselImages"
+        :id="'imageCarousel' + index"
+        :key="index"
+        class="carousel-item w-full"
+      >
+        <img :src="image.path" class="w-full">
       </div>
-      <div v-for="(image, index) in carouselImages" :key="index" class="flex justify-center w-full py-2 gap-2">
-        <a
-          href="#image{{index}}"
-          class="btn btn-circle btn-xs carousel-btn mt-5"
-          :class="{'btn-active': index===0, 'mr-5': index>0}"
-        />
-      </div>
-      <button class="btn btn-primary">
-        Button
-      </button>
+    </div>
+    <div class="flex justify-center w-full py-2 gap-2">
+      <a
+        v-for="(image, index) in carouselImages"
+        :key="'imageCarousel' + index"
+        :href="'#imageCarousel'+ index"
+        class="btn btn-circle btn-xs carousel-btn mt-5"
+        :class="{'btn-active': activeIndex===index, 'ml-5': index>0}"
+        @click="selectedIndex(index)"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import Vue from 'vue'
 
-export default {
+export default Vue.extend({
   props: {
     carouselImages: {
       type: Array,
-      default: () => [{ path: 'https://via.placeholder.com/150' }, { path: 'https://via.placeholder.com/150' }, { path: 'https://via.placeholder.com/150' }]
+      default: () => [{ path: 'https://via.placeholder.com/1920x1080' }, { path: 'https://via.placeholder.com/1920x1080' }, { path: 'https://via.placeholder.com/1920x1080' }]
+    },
+    imagesLength: {
+      type: Number,
+      default: 3
+    },
+    isComplex: {
+      type: Boolean,
+      default: true
     }
   },
   data () {
     return {
-      isActive: false
-
+      activeIndex: 0
+    }
+  },
+  methods: {
+    selectedIndex (index: number) {
+      this.activeIndex = index
     }
   }
-}
+})
 </script>
