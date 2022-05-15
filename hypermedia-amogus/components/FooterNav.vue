2ab94526c268f56bbc04dceb7d4576b1b927f775
Nicolò Sonnino <swagpino@gmail.com>
2022-05-16 01:45:22 +0200

    feat: better spacing

--- hypermedia-amogus/components/FooterNav.vue
+++ hypermedia-amogus/components/FooterNav.vue
@@ -2,131 +2,109 @@
   <footer class="footer w-full justify-center p-5">
     <div class="grid grid-cols-12">
       <div class="col-span-2 flex w-full" />
-      <div class="col-span-4 h-16 bg-base-100 w-full box-shadow-card rounded-3xl">
-        <div class="grid grid-rows-2 grid-cols-12 gap-2 w-full p-2 h-full">
-          <div class="col-span-4 row-span-2 flex items-center justify-center">
-            <div class="grid grid-cols-4">
-              <div class="col-span-2 flex items-center justify-center">
+      <div class="col-span-8 flex">
+        <div class="grid grid-cols-12">
+          <div class="col-span-7 h-16 bg-base-100 w-full box-shadow-card rounded-3xl">
+            <div class="grid grid-rows-2 grid-cols-5 gap-2 p-2 h-full flex justify-center">
+              <div class="row-span-2 flex items-center justify-center">
                 <IconsSiteLogo width="auto" height="75%" />
-              </div>
-              <div class="col-span-2 flex items-center justify-center">
                 <div class="divider divider-horizontal before:bg-warning after:bg-warning" />
               </div>
+              <NuxtLink class="flex justify-center" to="/home">
+                <p
+                  class="text-sm font-medium"
+                >
+                  Home
+                </p>
+              </NuxtLink>
+              <NuxtLink class="flex justify-center" to="itinerary">
+                <p
+                  class="text-sm font-medium"
+                >
+                  Itinerary
+                </p>
+              </NuxtLink>
+              <NuxtLink class="flex justify-center" to="about">
+                <p
+                  class="text-sm font-medium"
+                >
+                  About Us
+                </p>
+              </NuxtLink>
+              <NuxtLink class="flex justify-center" to="privacy">
+                <p
+                  class="text-sm font-medium"
+                >
+                  Privacy
+                  Policy
+                </p>
+              </NuxtLink>
+              <NuxtLink class="flex justify-center" to="events">
+                <p
+                  class="text-sm font-medium"
+                >
+                  Events
+                </p>
+              </NuxtLink>
+              <NuxtLink class="flex justify-center" to="pois">
+                <p
+                  class="text-sm font-medium"
+                >
+                  Point of
+                  Interest
+                </p>
+              </NuxtLink>
+              <NuxtLink class="flex justify-center" to="contacts">
+                <p
+                  class="text-sm font-medium"
+                >
+                  Contacts
+                </p>
+              </NuxtLink>
             </div>
           </div>
-          <div class="col-span-2">
-            <NuxtLink to="/home">
-              <p
-                class="text-sm font-medium"
-              >
-                Home
-              </p>
-            </NuxtLink>
-          </div>
-          <div class="col-span-2">
-            <NuxtLink to="itinerary">
-              <p
-                class="text-sm font-medium"
-              >
-                Itinerary
-              </p>
-            </NuxtLink>
-          </div>
-          <div class="col-span-2">
-            <NuxtLink to="about">
-              <p
-                class="text-sm font-medium"
-              >
-                About Us
-              </p>
-            </NuxtLink>
-          </div>
-          <div class="col-span-2 row-span-2">
-            <NuxtLink to="privacy">
-              <p
-                class="text-sm font-medium"
-              >
-                Privacy
-                Policy
-              </p>
-            </NuxtLink>
-          </div>
-          <div class="col-span-2">
-            <NuxtLink to="events">
-              <p
-                class="text-sm font-medium"
-              >
-                Events
-              </p>
-            </NuxtLink>
-          </div>
-          <div class="col-span-2">
-            <NuxtLink to="pois">
-              <p
-                class="text-sm font-medium"
-              >
-                Point of
-                Interest
-              </p>
-            </NuxtLink>
-          </div>
-          <div class="col-span-2">
-            <NuxtLink to="contacts">
-              <p
-                class="text-sm font-medium"
-              >
-                Contacts
-              </p>
-            </NuxtLink>
+          <div class="flex col-span-2" />
+          <div class="col-span-3 h-16 bg-base-100 w-full box-shadow-card rounded-3xl">
+            <div class="grid grid-cols-5 w-full p-2 h-full align-middle">
+              <div class="flex" />
+              <div class="col-span-3 grid grid-cols-4 grid-rows-2 w-full h-full">
+                <div class="col-span-4">
+                  <p class="text-center text-sm">
+                    Follow Us
+                  </p>
+                </div>
+                <div class="flex" />
+                <div class="flex col-span-2 justify-center gap-2 grid-cols-4">
+                  <IconsSocialFacebookIcon
+                    class="rounded-full icon-footer fill-white p-1"
+                    width="1.5rem"
+                    height="1.5rem"
+                  />
+                  <IconsSocialInstagramIcon
+                    class="rounded-full icon-footer fill-white p-1"
+                    width="1.5rem"
+                    height="1.5rem"
+                  />
+                  <IconsSocialTwitterIcon
+                    class="rounded-full icon-footer fill-white p-0"
+                    width="1.5rem"
+                    height="1.5rem"
+                  />
+                  <IconsSocialYoutubeIcon
+                    class="rounded-full icon-footer fill-white p-1"
+                    width="1.5rem"
+                    height="1.5rem"
+                  />
+                </div>
+
+                <div class="flex" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div class="flex w-full" />
-      <div class="col-span-2 h-16 bg-base-100 w-full box-shadow-card rounded-3xl">
-        <div class="grid grid-cols-5 w-full p-2 h-full align-middle">
-          <div class="flex" />
-          <div class="col-span-3 grid grid-cols-4 grid-rows-2 w-full h-full">
-            <div class="col-span-4">
-              <p class="text-center">
-                Follow Us
-              </p>
-            </div>
-            <div class="flex justify-center">
-              <IconsSocialFacebookIcon
-                class="rounded-full icon-footer fill-white p-1"
-                width="1.5rem"
-                height="1.5rem"
-              />
-            </div>
-            <div class="flex justify-center">
-              <IconsSocialInstagramIcon
-                class="rounded-full icon-footer fill-white p-1"
-                width="1.5rem"
-                height="1.5rem"
-              />
-            </div>
 
-            <div class="flex justify-center">
-              <IconsSocialTwitterIcon
-                class="rounded-full icon-footer fill-white p-0"
-                width="1.5rem"
-                height="1.5rem"
-              />
-            </div>
-
-            <div class="flex justify-center">
-              <IconsSocialYoutubeIcon
-                class="rounded-full icon-footer fill-white p-1"
-                width="1.5rem"
-                height="1.5rem"
-              />
-            </div>
-
-            <div class="flex" />
-          </div>
-        </div>
-      </div>
-      <div class="col-span-3 flex w-full" />
+      <div class="col-span-2 flex w-full" />
     </div>
   </footer>
 </template>
