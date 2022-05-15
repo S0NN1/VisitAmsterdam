05e83e98c42aa5af87cc84cf6c5dc2b64ec84315
Nicolò Sonnino <swagpino@gmail.com>
2022-05-15 19:12:31 +0200

    feat: footer

--- hypermedia-amogus/components/FooterNav.vue
+++ hypermedia-amogus/components/FooterNav.vue
@@ -1,47 +1,51 @@
 <template>
-  <footer class="footer w-full justify-center">
-    <div class="grid-cols-12">
+  <footer class="footer w-full justify-center p-5">
+    <div class="grid grid-cols-12">
       <div class="col-span-2 flex w-full" />
-      <div class="col-span-4 bg-base-100 w-full drop-shadow-lg rounded-xl card">
-        <div class="grid grid-rows-2 grid-cols-12 gap-2 w-full">
-          <div class="col-span-3 row-span-2">
-            <svg id="a" class="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2500 735.36">
-              <defs>
-                <style>.b {
-                  fill: #f05a28;
-                  }</style>
-              </defs>
-              <path
-                class="b"
-                d="M2353.24,735.36l-220.53-220.53-220.53,220.53-147.15-147.15,220.53-220.53-220.53-220.53L1912.18,0l220.53,220.53L2353.24,0l146.76,147.55-220.53,220.53,220.53,220.53-146.76,146.76Zm-1765.43,0l-220.53-220.53-220.14,220.53L0,588.21l220.53-220.53L0,147.55,147.15,.39l220.53,220.53L588.21,.39l146.76,147.15-220.53,220.53,220.53,220.53-147.15,146.76Zm883.3,0l-220.53-220.53-220.14,220.53-147.15-147.15,220.53-220.53-220.53-220.53L1030.46,0l220.53,220.53L1471.52,0l146.76,147.55-220.53,220.53,220.53,220.53-147.15,146.76Z"
-              />
-            </svg>
+      <div class="col-span-4 h-16 bg-base-100 w-full box-shadow-card rounded-3xl">
+        <div class="grid grid-rows-2 grid-cols-12 gap-2 w-full p-2 h-full">
+          <div class="col-span-4 row-span-2 flex items-center justify-center">
+            <div class="grid grid-cols-4">
+              <div class="col-span-2 flex items-center justify-center">
+                <IconsSiteLogo width="auto" height="75%" />
+              </div>
+              <div class="col-span-2 flex items-center justify-center">
+                <div class="divider divider-horizontal before:bg-warning after:bg-warning" />
+              </div>
+            </div>
           </div>
-          <div class="w-full row-span-2" />
           <div class="col-span-2">
             <NuxtLink to="/home">
-              <p :class="$nuxt.$route.name==='home' ? 'underline decoration-2 decoration-sky-500' : ''">
+              <p
+                class="text-sm font-medium"
+              >
                 Home
               </p>
             </NuxtLink>
           </div>
           <div class="col-span-2">
             <NuxtLink to="itinerary">
-              <p :class="$nuxt.$route.name==='itinerary' ? 'underline decoration-2 decoration-sky-500' : ''">
+              <p
+                class="text-sm font-medium"
+              >
                 Itinerary
               </p>
             </NuxtLink>
           </div>
           <div class="col-span-2">
             <NuxtLink to="about">
-              <p :class="$nuxt.$route.name==='about' ? 'underline decoration-2 decoration-sky-500' : ''">
+              <p
+                class="text-sm font-medium"
+              >
                 About Us
               </p>
             </NuxtLink>
           </div>
           <div class="col-span-2 row-span-2">
             <NuxtLink to="privacy">
-              <p :class="$nuxt.$route.name==='privacy' ? 'underline decoration-2 decoration-sky-500' : ''">
+              <p
+                class="text-sm font-medium"
+              >
                 Privacy
                 Policy
               </p>
@@ -49,14 +53,18 @@
           </div>
           <div class="col-span-2">
             <NuxtLink to="events">
-              <p :class="$nuxt.$route.name==='events' ? 'underline decoration-2 decoration-sky-500' : ''">
+              <p
+                class="text-sm font-medium"
+              >
                 Events
               </p>
             </NuxtLink>
           </div>
           <div class="col-span-2">
             <NuxtLink to="pois">
-              <p :class="$nuxt.$route.name==='pois' ? 'underline decoration-2 decoration-sky-500' : ''">
+              <p
+                class="text-sm font-medium"
+              >
                 Point of
                 Interest
               </p>
@@ -64,7 +72,9 @@
           </div>
           <div class="col-span-2">
             <NuxtLink to="contacts">
-              <p :class="$nuxt.$route.name==='contacts' ? 'underline decoration-2 decoration-sky-500' : ''">
+              <p
+                class="text-sm font-medium"
+              >
                 Contacts
               </p>
             </NuxtLink>
@@ -72,29 +82,40 @@
         </div>
       </div>
       <div class="flex w-full" />
-      <div class="col-span-3 flex w-full">
-        <div class="p-2 grid grid-rows-2 grid-flow-col gap-2 h-full">
-          <div class="text-center">
-            <p>Follow Us</p>
-          </div>
-          <div class="w-fit mx-auto flex items-center fill-black">
-            <div class="mx-4">
-              <IconsSocialFacebookIcon width="1.5rem" height="1.5rem" />
+      <div class="col-span-2 h-16 bg-base-100 w-full box-shadow-card rounded-3xl">
+        <div class="grid grid-cols-5 w-full p-2 h-full">
+          <div class="flex" />
+          <div class="col-span-3 grid grid-cols-4 grid-rows-2 w-full h-full">
+            <div class="col-span-4">
+              <p class="text-center">
+                Follow Us
+              </p>
             </div>
-            <div class="mx-4">
-              <IconsSocialInstagramIcon width="1.5rem" height="1.5rem" />
-            </div>
-            <div class="mx-4">
-              <IconsSocialTwitterIcon width="1.5rem" height="1.5rem" />
-            </div>
-            <div class="mx-4">
-              <IconsSocialYoutubeIcon width="1.5rem" height="1.5rem" />
-            </div>
+            <IconsSocialFacebookIcon
+              class="rounded-full icon-footer fill-white p-0.5"
+              width="1.5rem"
+              height="1.5rem"
+            />
+            <IconsSocialInstagramIcon
+              class="rounded-full icon-footer fill-white p-0.5"
+              width="1.5rem"
+              height="1.5rem"
+            />
+            <IconsSocialTwitterIcon
+              class="rounded-full icon-footer fill-white p-0.5"
+              width="1.5rem"
+              height="1.5rem"
+            />
+            <IconsSocialYoutubeIcon
+              class="rounded-full icon-footer fill-white p-0.5"
+              width="1.5rem"
+              height="1.5rem"
+            />
+            <div class="flex" />
           </div>
         </div>
       </div>
-      <div class="col-span-2 flex w-full" />
-    </div>
+      <div class="col-span-3 flex w-full" />
     </div>
   </footer>
 </template>
