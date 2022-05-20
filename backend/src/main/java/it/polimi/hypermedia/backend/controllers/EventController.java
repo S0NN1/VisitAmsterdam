2ec35ae86dbac1b7f64ad55636885fca849eca1d
Luca Pirovano <luca.pirovano.98@gmail.com>
2022-05-20 09:31:56 +0200

    fix: event exceptions

--- backend/src/main/java/it/polimi/hypermedia/backend/controllers/EventController.java
+++ backend/src/main/java/it/polimi/hypermedia/backend/controllers/EventController.java
@@ -1,11 +1,13 @@
 package it.polimi.hypermedia.backend.controllers;
 
 import it.polimi.hypermedia.backend.entities.Event;
+import it.polimi.hypermedia.backend.exception.EventNotFoundException;
 import it.polimi.hypermedia.backend.repositories.EventRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/event")
@@ -28,7 +30,12 @@ public class EventController {
 
     @GetMapping(value = "/getById")
     public Event getById(@RequestParam long id) {
-        return eventRepository.findById(id).get();
+        Optional<Event> event = eventRepository.findById(id);
+        if(event.isPresent()) {
+            return event.get();
+        } else {
+            throw new EventNotFoundException();
+        }
     }
 
     @GetMapping(value = "/getUpcoming")
