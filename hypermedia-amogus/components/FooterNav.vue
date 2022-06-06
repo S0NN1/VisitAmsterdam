6e6c577387cdb64565dd02105ebb2d7d4c07470b
Nicolò Sonnino <swagpino@gmail.com>
2022-06-06 12:08:01 +0200

    fix: drawer and footer spacing

--- hypermedia-amogus/components/FooterNav.vue
+++ hypermedia-amogus/components/FooterNav.vue
@@ -1,23 +1,23 @@
 <template>
   <footer class="footer w-full justify-center p-5">
     <div class="grid grid-cols-12">
-      <div :class="!mobile ? 'col-span-2' : ''" class="flex w-full" />
-      <div :class="!mobile ? 'col-span-8' : 'col-span-10'" class="flex w-full">
-        <div v-if="!mobile" class="grid grid-cols-12">
+      <div class="flex w-full md:col-span-2" />
+      <div class="flex w-full col-span-10 md:col-span-8">
+        <div v-if="!mobile" class="flex grid grid-cols-12 w-full">
           <div class="col-span-8 h-24 bg-base-100 w-full box-shadow-card rounded-xl">
-            <div class="grid grid-rows-2 grid-cols-5 gap-y-0 gap-x-3 py-5 px-2 h-full flex">
+            <div class="grid grid-rows-2 grid-cols-5 gap-y-0 gap-x-3 py-3 px-2 h-full flex">
               <div class="flex  h-full  row-span-2 col-span-2 items-center">
                 <IconsSiteLogo class="pr-3 mr-3 border-r-2 border-r-warning w-6/12" width="auto" height="75%" />
 
-                <div class="flex w-6/12 h-full grid grid-rows-2 gap-y-0 gap-x-3">
-                  <NuxtLink class="flex ml-5" to="/home">
+                <div class="flex w-6/12 h-full w-full grid grid-rows-2 gap-y-0 gap-x-3">
+                  <NuxtLink class="flex w-full ml-5" to="/home">
                     <p
                       class="text-sm font-medium truncate text-error"
                     >
                       <b>Home</b>
                     </p>
                   </NuxtLink>
-                  <NuxtLink class="flex ml-5" to="/events">
+                  <NuxtLink class="flex w-full ml-5" to="/events">
                     <p
                       class="text-sm font-medium truncate"
                     >
@@ -52,7 +52,7 @@
                 <p
                   class="text-sm font-medium truncate"
                 >
-                  Points of<br>
+                  Points of
                   Interest
                 </p>
               </NuxtLink>
@@ -74,7 +74,7 @@
             </div>
           </div>
           <div class="flex col-span-1" />
-          <div class="col-span-3 h-16 bg-base-100 w-full box-shadow-card rounded-xl">
+          <div class="col-span-3 h-24 bg-base-100 w-full box-shadow-card rounded-xl">
             <div class="grid grid-cols-5 w-full p-1 h-full align-middle">
               <div class="flex" />
               <div class="col-span-3 grid grid-cols-6 grid-rows-3 w-full h-full items-center">
@@ -110,66 +110,66 @@
           </div>
         </div>
         <div v-else class="h-72 bg-base-100 w-full box-shadow-card rounded-xl">
-          <div class="grid grid-cols-2 grid-rows-7 gap-y-2 py-5 px-10">
+          <div class="flex w-full h-full grid grid-cols-2 grid-rows-7 gap-y-2 py-5 px-10">
             <NuxtLink class="flex w-full" to="/home">
               <p
-                class="text-sm font-medium truncate text-error"
+                class="text-base font-medium truncate text-error"
               >
                 <b>Home</b>
               </p>
             </NuxtLink>
             <NuxtLink class="flex w-full" to="/itineraries">
               <p
-                class="text-sm font-medium truncate"
+                class="text-base font-medium truncate"
               >
                 Itineraries
               </p>
             </NuxtLink>
             <NuxtLink class="flex w-full" to="/events">
               <p
-                class="text-sm font-medium truncate"
+                class="text-base font-medium truncate"
               >
                 Events
               </p>
             </NuxtLink>
             <NuxtLink class="flex w-full" to="/points-of-interest">
               <p
-                class="text-sm font-medium truncate"
+                class="text-base font-medium truncate"
               >
-                Points of<br>
+                Points of
                 Interest
               </p>
             </NuxtLink>
             <NuxtLink class="flex w-full" to="/services">
               <p
-                class="text-sm font-medium truncate"
+                class="text-base font-medium truncate"
               >
                 Services
               </p>
             </NuxtLink>
             <NuxtLink class="flex w-full" to="/contacts">
               <p
-                class="text-sm font-medium truncate"
+                class="text-base font-medium truncate"
               >
                 Contacts
               </p>
             </NuxtLink>
             <NuxtLink class="flex w-full" to="/about">
               <p
-                class="text-sm font-medium truncate"
+                class="text-base font-medium truncate"
               >
                 About Us
               </p>
             </NuxtLink>
             <NuxtLink class="flex w-full" to="tos">
               <p
-                class="text-sm font-medium truncate"
+                class="text-base font-medium truncate"
               >
-                Privacy<br>
+                Privacy
                 Policy
               </p>
             </NuxtLink>
-            <div class="row-span-2 col-span-2 grid grid-rows-3 grid-cols-6 w-full mt-3 pt-3  border-t-2 border-neutral">
+            <div class="row-span-2 col-span-2 grid grid-rows-3 grid-cols-6 w-full border-t-2 border-neutral mt-2 pt-2">
               <div class="col-span-6">
                 <p class="text-center text-sm truncate">
                   Follow Us
@@ -204,6 +204,7 @@
           <div :class="!mobile ? 'col-span-2' : ''" class="flex w-full" />
         </div>
       </div>
+      <div class="flex w-full md:col-span-2" />
     </div>
   </footer>
 </template>
@@ -235,4 +236,9 @@ export default Vue.extend({
     transparent 100%
   );
 }
+
+.icon-footer {
+  background-color: #263049;
+}
+
 </style>
--- hypermedia-amogus/pages/index.vue
+++ hypermedia-amogus/pages/index.vue
@@ -6,19 +6,21 @@
         <!-- Navbar -->
         <div class="w-full navbar bg-none">
           <div class="flex-1 sm:px-2 sm:mx-2">
-            <span class="flex text-2xl text-white">
-              <svg width="2.3rem" height="2.3rem" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M7.17158 14.2426C6.78106 14.6332 6.14789 14.6332 5.75737 14.2426C5.36684 13.8521 5.36684 13.2189 5.75737 12.8284L12.8284 5.75735C13.219 5.36682 13.8521 5.36682 14.2427 5.75735C14.6332 6.14787 14.6332 6.78104 14.2427 7.17156L7.17158 14.2426Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M5.75737 7.17152C5.36684 6.781 5.36684 6.14783 5.75737 5.75731C6.14789 5.36679 6.78106 5.36679 7.17158 5.75731L14.2427 12.8284C14.6332 13.2189 14.6332 13.8521 14.2427 14.2426C13.8521 14.6331 13.219 14.6331 12.8284 14.2426L5.75737 7.17152Z"
-                  fill="currentColor"
-                />
-              </svg>
-              <b>Amsterdam</b>
-            </span>
+            <NuxtLink to="/" class="w-full">
+              <span class="flex text-2xl text-white">
+                <svg width="2.3rem" height="2.3rem" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M7.17158 14.2426C6.78106 14.6332 6.14789 14.6332 5.75737 14.2426C5.36684 13.8521 5.36684 13.2189 5.75737 12.8284L12.8284 5.75735C13.219 5.36682 13.8521 5.36682 14.2427 5.75735C14.6332 6.14787 14.6332 6.78104 14.2427 7.17156L7.17158 14.2426Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M5.75737 7.17152C5.36684 6.781 5.36684 6.14783 5.75737 5.75731C6.14789 5.36679 6.78106 5.36679 7.17158 5.75731L14.2427 12.8284C14.6332 13.2189 14.6332 13.8521 14.2427 14.2426C13.8521 14.6331 13.219 14.6331 12.8284 14.2426L5.75737 7.17152Z"
+                    fill="currentColor"
+                  />
+                </svg>
+                <b>Amsterdam</b>
+              </span>
+            </NuxtLink>
           </div>
           <div class="flex-none sm:hidden">
             <label for="my-drawer-3" class="btn btn-square btn-ghost">
@@ -146,22 +148,22 @@
       </div>
       <div class="drawer-side">
         <label for="my-drawer-3" class="drawer-overlay" />
-        <ul class="menu p-4 overflow-y-auto w-80 bg-primary rounded-tl-3xl items-center">
-          <li class="grid grid-cols-3">
+        <ul class="menu overflow-y-auto w-80 bg-primary rounded-tl-3xl items-center">
+          <li class="flex w-full grid grid-cols-3 p-0">
             <div class="flex justify-start">
               <label
                 for="my-drawer-3"
                 class="flex btn btn-primary border-none text-white fill-white drawer-button pl-0"
               >
-                <IconsCloseIcon class="icon-shadow fill-white" width="3rem" height="3rem" />
+                <IconsCloseIcon class="icon-shadow fill-white" width="2rem" height="2rem" />
               </label>
             </div>
             <div class="flex col-span-2 justify-end">
               <button class="flex items-center justify-center text-white fill-white mr-4">
-                <IconsWorldwideIcon class="icon-shadow fill-white" width="2rem" height="2rem" />
+                <IconsWorldwideIcon class="icon-shadow fill-white" width="1.5rem" height="1.5rem" />
               </button>
               <button class="flex items-center justify-center text-white fill-white">
-                <IconsUserIcon class="icon-shadow fill-white" width="2rem" height="2rem" />
+                <IconsUserIcon class="icon-shadow fill-white" width="1.5rem" height="1.5rem" />
               </button>
             </div>
           </li>
@@ -170,7 +172,7 @@
           <li class="sidebar-li w-full">
             <button class="flex items-center justify-center text-white">
               <div
-                class="flex appearance-none my-1 sm:max-w-screen-sm text-stone-900 border-b-2 border-[#601616] border-opacity-20"
+                class="flex appearance-none my-1 sm:max-w-screen-sm text-stone-900"
               >
                 <button class="flex items-center justify-center text-white fill-white">
                   <IconsSearchIcon />
@@ -183,6 +185,7 @@
               </div>
             </button>
           </li>
+          <div class="border-b-2 border-[#601616] border-opacity-20 h-2 w-11/12" />
           <li class="sidebar-li w-full">
             <NuxtLink to="/home" class="w-full justify-center">
               <button class="flex items-center justify-center text-white">
@@ -248,8 +251,8 @@
           </li>
           <div class="border-b-2 border-[#601616] border-opacity-20 h-2 w-11/12" />
           <li class="flex h-full w-full align-baseline justify-center">
-            <div class=" flex h-full grid grid-rows-3">
-              <div class="flex row-span-2" />
+            <div class=" flex h-full grid grid-rows-2">
+              <div class="flex " />
               <div class=" grid grid-cols-4 gap-0 w-full h-full items-center">
                 <IconsSocialFacebookIcon
                   class="rounded-full icon-footer fill-primary p-1.5 aspect-square mx-auto my-auto h-fit"
@@ -300,8 +303,12 @@ li {
 }
 
 .sidebar-li {
-  @apply text-2xl;
+  @apply text-base;
   @apply font-bold;
   line-height: unset;
+}
+
+.icon-footer {
+  @apply bg-white
 }
 </style>
